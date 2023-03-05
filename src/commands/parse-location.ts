7a2cb0a9fafa80b6e794da4ccf88959488377d6f
Michael Belousov <mike.belousov@bentley.com>
2023-03-05 18:58:50 -0500

    fix and simplify location parsing

--- src/commands/parse-location.ts
+++ src/commands/parse-location.ts
@@ -7,34 +7,23 @@ const debugJsdbg = debug("jsdbg");
 
 export async function parseLocation(src: string, ctx: DebugContext): Promise<Location> {
   src = src.trim();
-  const match = /(?<fileOrRef>.+:)?(?<line>\d+)?(:(?<col>\d+))?$/.exec(src);
+  const match = /(?<line>\d+)?(:(?<col>\d+))?$/.exec(src);
   assert(match?.groups, "bad location syntax");
 
-  const [fileOrRef, line, col]
-    = match.groups.col  ? [match.groups.fileOrRef, match.groups.line, match.groups.col]
-    : (
-      match.groups.line
-      // NOTE: can put an extra group in the original regex to parse whether it's a number then
-        ? /\d+/.test(match.groups.fileOrRef)
-          ? [undefined, +match.groups.fileOrRef, +match.groups.line]
-          : [match.groups.fileOrRef, +match.groups.line, 0]
-        : [match.groups.fileOrRef, +match.groups.line, 0]
-    )
+  const fileOrRef = src.slice(0, -match[0].length-1)
+  const col = +(match.groups?.col ?? 0);
+  const line = +(match.groups?.line ?? 0);
 
-  // TODO: fix gross
-  //const col = +(match.groups?.col ?? 0);
-  //const line = +(match.groups?.line ?? match.groups?.col ?? 0);
+  debug("jsdbg:parse")({ fileOrRef, col, line });
 
-  debug("jsdbg:parse")(match);
-
-  const probablyFile = match?.groups?.fileOrRef && /[/\\]/.test(match.groups.fileOrRef);
-  const probablyRef = match?.groups?.fileOrRef && !probablyFile && !/\d+/.test(src);
+  const probablyFile = fileOrRef && /[/\\]/.test(fileOrRef);
+  const probablyRef = fileOrRef && !probablyFile;
 
   if (probablyRef)
     throw Error("breaking on function-like reference not yet supported");
 
   const urls = probablyFile
-    ? await ctx.engine.findLoadedFile(match.groups!.fileOrRef)
+    ? await ctx.engine.findLoadedFile(fileOrRef)
     : await ctx.engine.getLocation().then(l => l ? [l.sourceUrl] : []);
 
   // TODO: break on all results
